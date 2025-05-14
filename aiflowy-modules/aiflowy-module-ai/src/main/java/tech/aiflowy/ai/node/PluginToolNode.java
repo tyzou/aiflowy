8b3e2ba0e9fde5f63a3a683f6df127cf2e7d1129
Michael Yang <fuhai999@gmail.com>
2025-05-14 14:09:56 +0800

    refactor: 优化 PluginToolNode 的执行方法

--- aiflowy-modules/aiflowy-module-ai/src/main/java/tech/aiflowy/ai/node/PluginToolNode.java
+++ aiflowy-modules/aiflowy-module-ai/src/main/java/tech/aiflowy/ai/node/PluginToolNode.java
@@ -2,18 +2,17 @@ package tech.aiflowy.ai.node;
 
 import com.agentsflex.core.chain.Chain;
 import com.agentsflex.core.chain.node.BaseNode;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.agentsflex.core.llm.functions.Function;
+import com.alibaba.fastjson.JSON;
 import tech.aiflowy.ai.entity.AiPluginTool;
 import tech.aiflowy.ai.service.AiPluginToolService;
 import tech.aiflowy.common.util.SpringContextUtil;
 
 import java.math.BigInteger;
+import java.util.Collections;
 import java.util.Map;
 
 public class PluginToolNode extends BaseNode {
-
-    private static final Logger log = LoggerFactory.getLogger(PluginToolNode.class);
 
     private final BigInteger pluginId;
 
@@ -21,12 +20,30 @@ public class PluginToolNode extends BaseNode {
         this.pluginId = pluginId;
     }
 
-    @Override
     @SuppressWarnings("unchecked")
+    @Override
     protected Map<String, Object> execute(Chain chain) {
         Map<String, Object> map = chain.getParameterValues(this);
         AiPluginToolService bean = SpringContextUtil.getBean(AiPluginToolService.class);
         AiPluginTool tool = bean.getById(pluginId);
-        return (Map<String, Object>) tool.toFunction().invoke(map);
+        if (tool == null) {
+            return Collections.emptyMap();
+        }
+
+        Function function = tool.toFunction();
+        if (function == null) {
+            return Collections.emptyMap();
+        }
+
+        Object result = function.invoke(map);
+        if (result == null) {
+            return Collections.emptyMap();
+        }
+
+        if (result instanceof Map) {
+            return (Map<String, Object>) result;
+        }
+
+        return JSON.parseObject(JSON.toJSONString(result), Map.class);
     }
 }
