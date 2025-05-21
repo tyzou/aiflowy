37e2630009bce127a1e91bee6c73cd1b4e0d569c
zhangjinyang <409225776@qq.com>
2025-05-21 17:07:30 +0800

    build: v1.0.3 release (^.^)YYa!!

--- aiflowy-commons/aiflowy-common-base/src/main/java/tech/aiflowy/common/Consts.java
+++ aiflowy-commons/aiflowy-common-base/src/main/java/tech/aiflowy/common/Consts.java
@@ -1,7 +1,7 @@
 package tech.aiflowy.common;
 
 public class Consts {
-    public static final String VERSION = "v1.0.2";
+    public static final String VERSION = "v1.0.3";
 
     public static final String REQ_ATTR_ACCOUNT_ID = "loginAccountId";
     public static final String JWT_ATTR_SESSION_ID = "sessionId";
--- aiflowy-ui-react/src/pages/commons/login.tsx
+++ aiflowy-ui-react/src/pages/commons/login.tsx
@@ -161,7 +161,7 @@ const Login: React.FC = () => {
                 <a href="https://aiflowy.tech " style={{ color: "#fff" }} target="_blank">
                     AIFlowy.tech
                 </a>{" "}
-                (v1.0.2)
+                (v1.0.3)
             </div>
         </div>
     ) : (
