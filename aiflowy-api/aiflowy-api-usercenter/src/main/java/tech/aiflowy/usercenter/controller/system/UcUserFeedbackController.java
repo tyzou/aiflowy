f63c8fe8be8979bbf5aff69a399c9ae354d68621
18586361686 <1207669650@qq.com>
2025-12-31 17:28:01 +0800

    chore: chore code

--- aiflowy-api/aiflowy-api-usercenter/src/main/java/tech/aiflowy/usercenter/controller/system/UcUserFeedbackController.java
+++ aiflowy-api/aiflowy-api-usercenter/src/main/java/tech/aiflowy/usercenter/controller/system/UcUserFeedbackController.java
@@ -1,9 +1,13 @@
 package tech.aiflowy.usercenter.controller.system;
 
+import cn.dev33.satoken.annotation.SaIgnore;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tech.aiflowy.common.annotation.UsePermission;
+import tech.aiflowy.common.domain.Result;
 import tech.aiflowy.common.web.controller.BaseCurdController;
+import tech.aiflowy.common.web.jsonbody.JsonBody;
 import tech.aiflowy.system.entity.SysUserFeedback;
 import tech.aiflowy.system.service.SysUserFeedbackService;
 
@@ -18,6 +22,13 @@ import tech.aiflowy.system.service.SysUserFeedbackService;
 @RequestMapping("/userCenter/sysUserFeedback")
 @UsePermission(moduleName = "/api/v1/sysUserFeedback")
 public class UcUserFeedbackController extends BaseCurdController<SysUserFeedbackService, SysUserFeedback> {
+
+    @PostMapping("/save")
+    @SaIgnore
+    public Result<?> save(@JsonBody SysUserFeedback entity) {
+        return super.save(entity);
+    }
+
     public UcUserFeedbackController(SysUserFeedbackService service) {
         super(service);
     }
